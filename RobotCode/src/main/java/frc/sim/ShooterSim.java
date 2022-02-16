a433c3ba4097eef084f35302512154d9b500a712
Chris Gerth <chrisgerth010592@gmail.com>
2022-02-15 19:15:20 -0600

    wip updates to make shooter model match real-world performance

--- RobotCode/src/main/java/frc/sim/ShooterSim.java
+++ RobotCode/src/main/java/frc/sim/ShooterSim.java
@@ -14,9 +14,14 @@ public class ShooterSim {
 
     SimSmartMotor shooterMotor;
 
-    private final double SHOOTER_GEAR_RATIO = 1.0;
-    private final double SHOOTER_WHEEL_MASS_kg = Units.lbsToKilograms(5.0);
-    private final double SHOOTER_WHEEL_RADIUS_m = Units.inchesToMeters(6.0);
+    private final double SHOOTER_GEAR_RATIO = 3.0/2.0;
+    //One Fairlane
+    private final double SHOOTER_WHEEL_BOTTOM_MASS_kg = Units.lbsToKilograms(1.56);
+    private final double SHOOTER_WHEEL_BOTTOM_RADIUS_m = Units.inchesToMeters(4.0/2);
+    //Two Colsons
+    private final double SHOOTER_WHEEL_TOP_MASS_kg = Units.lbsToKilograms(0.64);
+    private final double SHOOTER_WHEEL_TOP_RADIUS_m = Units.inchesToMeters(3.5/2);
+
 
     DCMotor drivingMotor = DCMotor.getNEO(1);
 
@@ -28,8 +33,10 @@ public class ShooterSim {
 
     public ShooterSim(){
 
-        double moi = 0.5 * SHOOTER_WHEEL_MASS_kg * SHOOTER_WHEEL_RADIUS_m * SHOOTER_WHEEL_RADIUS_m;
-        motorWithRotatingMass = new FlywheelSim(drivingMotor, SHOOTER_GEAR_RATIO, moi);
+        double moi_top = 0.5 * SHOOTER_WHEEL_TOP_MASS_kg * SHOOTER_WHEEL_TOP_RADIUS_m * SHOOTER_WHEEL_TOP_RADIUS_m;
+        double moi_bottom = 0.5 * SHOOTER_WHEEL_BOTTOM_MASS_kg * SHOOTER_WHEEL_BOTTOM_RADIUS_m * SHOOTER_WHEEL_BOTTOM_RADIUS_m;
+
+        motorWithRotatingMass = new FlywheelSim(drivingMotor, SHOOTER_GEAR_RATIO, moi_top + moi_bottom);
         shooterMotor = (SimSmartMotor) SimDeviceBanks.getCANDevice(Constants.SHOOTER_MOTOR_CANID);
     }
 
