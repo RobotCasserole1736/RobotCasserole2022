f2141aa9552a66d44c2ded5d1043d1936c9fd19c
Chris Gerth <chrisgerth010592@gmail.com>
2022-02-15 21:02:15 -0600

    quick cleanups

--- RobotCode/src/main/java/frc/robot/Ballcolordetector.java
+++ RobotCode/src/main/java/frc/robot/Ballcolordetector.java
@@ -2,42 +2,46 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.util.Color;
+import frc.lib.Signal.Annotations.Signal;
 
 import com.revrobotics.ColorMatch;
 import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
 
 public class Ballcolordetector {
-    private final I2C.Port i2cPort = I2C.Port.kOnboard;
+    private final I2C.Port i2cPort = I2C.Port.kMXP;
     private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
 
     private final ColorMatch m_colorMatcher = new ColorMatch();
 
     private final Color kBlueTarget = new Color(0.143, 0.427, 0.429);
-    private final Color kGreenTarget = new Color(0.197, 0.561, 0.240);
     private final Color kRedTarget = new Color(0.561, 0.232, 0.114);
-    private final Color kYellowTarget = new Color(0.361, 0.524, 0.113);
+
+    @Signal
+    boolean blueDetected = false;
+    @Signal
+    boolean redDetected = false;
 
     public Ballcolordetector() {
         m_colorMatcher.addColorMatch(kBlueTarget);
-        m_colorMatcher.addColorMatch(kGreenTarget);
         m_colorMatcher.addColorMatch(kRedTarget);
-        m_colorMatcher.addColorMatch(kYellowTarget);
+        m_colorMatcher.setConfidenceThreshold(0.5);
     }
 
     public void update() {
         Color detectedColor = m_colorSensor.getColor();
 
-        String colorString;
         ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
 
         if (match.color == kBlueTarget) {
-            colorString = "Blue";
+            blueDetected = true;
+            redDetected = false;
         } else if (match.color == kRedTarget) {
-            colorString = "Red";
+            blueDetected = false;
+            redDetected = true;
         } else {
-            colorString = "Unknown";
+            blueDetected = false;
+            redDetected = false;
         }
-        System.out.println(colorString);
     }
 }
