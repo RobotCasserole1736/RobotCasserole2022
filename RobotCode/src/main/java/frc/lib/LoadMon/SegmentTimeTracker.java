bf461a26d1e2906b52e6079f60dc5481e3fba92b
Chris Gerth <chrisgerth010592@gmail.com>
2022-02-28 15:50:15 -0600

    Forgot to add new segment time tracker

---
+++ RobotCode/src/main/java/frc/lib/LoadMon/SegmentTimeTracker.java
@@ -0,0 +1,46 @@
+package frc.lib.LoadMon;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Tracer;
+import frc.lib.Signal.Annotations.Signal;
+
+public class SegmentTimeTracker {
+    private Tracer trace;
+    private String name;
+
+    private final double max_dur_sec;
+
+
+    @Signal(units="sec")
+    double loopPeriodSec;
+    @Signal(units="sec")
+    double loopDurationSec;
+
+    double startTimeSec;
+
+    
+    public SegmentTimeTracker(String name, double max_dur_sec){
+        trace = new Tracer();
+        this.name = name;
+        this.max_dur_sec = max_dur_sec;
+    }
+
+    public void start(){
+        loopPeriodSec = Timer.getFPGATimestamp() - startTimeSec;
+        startTimeSec = Timer.getFPGATimestamp();
+        trace.clearEpochs();
+    }
+
+    public void mark(String epochName){
+        trace.addEpoch(epochName);
+    }
+
+    public void end(){
+        loopDurationSec = Timer.getFPGATimestamp() - startTimeSec;
+        if (loopDurationSec > max_dur_sec) {
+            System.out.println(name + " Long Loop Detected:");
+            trace.printEpochs(System.out::println);
+        }
+    }
+
+}
