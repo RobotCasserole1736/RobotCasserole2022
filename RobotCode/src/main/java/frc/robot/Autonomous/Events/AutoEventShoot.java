3eb2902487fe06b1734a77f2167b681392a867ea
ElliottHicks1125 <elliott41414@gmal.com>
2022-03-13 15:48:24 -0500

    More auto changes

--- RobotCode/src/main/java/frc/robot/Autonomous/Events/AutoEventShoot.java
+++ RobotCode/src/main/java/frc/robot/Autonomous/Events/AutoEventShoot.java
@@ -3,6 +3,7 @@ package frc.robot.Autonomous.Events;
 import edu.wpi.first.wpilibj.Timer;
 import frc.lib.AutoSequencer.AutoEvent;
 import frc.robot.Shooter;
+import frc.robot.Shooter.shooterFeedCmdState;
 import frc.robot.Shooter.shooterLaunchState;
 
 public class AutoEventShoot extends AutoEvent {
@@ -18,6 +19,7 @@ public class AutoEventShoot extends AutoEvent {
 	public void userStart() {
 		endTime = Timer.getFPGATimestamp() + duration_s;
         Shooter.getInstance().setRun(shooterLaunchState.HIGH_GOAL);
+		Shooter.getInstance().setFeed(shooterFeedCmdState.FEED);
 	}
 
 	@Override
@@ -25,12 +27,14 @@ public class AutoEventShoot extends AutoEvent {
 		completed = (Timer.getFPGATimestamp() > endTime);
 		if (completed){
 			Shooter.getInstance().setRun(shooterLaunchState.STOP);
+			Shooter.getInstance().setFeed(shooterFeedCmdState.STOP);
 			}
 	}
 
 	@Override
 	public void userForceStop() {
         Shooter.getInstance().setRun(shooterLaunchState.STOP);
+		Shooter.getInstance().setFeed(shooterFeedCmdState.STOP);
 	}
 
 	@Override
--- RobotCode/src/main/java/frc/robot/Drivetrain/DrivetrainControl.java
+++ RobotCode/src/main/java/frc/robot/Drivetrain/DrivetrainControl.java
@@ -213,7 +213,7 @@ public class DrivetrainControl {
 
 
         pe.update();
-
+ 
     }
 
 
